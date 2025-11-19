323d8df6b88fe6b4a37f94aa22ac34d793e7b02e
renanmarquespereira <163781666+renanmarquespereira@users.noreply.github.com>
2025-11-19 07:02:58 -0300

    Atualizar o urls.py

--- loja/urls.py
+++ loja/urls.py
@@ -3,13 +3,15 @@ from . import views
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
+    #Home
     path('', views.homepage, name='homepage'),
+    
+    #Login
     path('login/', views.fazer_login, name='fazer_login'),
     path('logout/', views.fazer_logout, name='fazer_logout'),
-    path('criarconta/', views.criar_conta, name='criar_conta'),
-    path('minhaconta/', views.minha_conta, name='minha_conta'),
+    
+    #Loja
     path('meuspedidos/', views.meus_pedidos, name='meus_pedidos'),
-    path('recuperarsenha/', views.recuperar_senha, name='recuperar_senha'),
     path('loja/', views.loja, name='loja'),
     path('loja/<str:filtro>/', views.loja, name='loja'),
     path('loja/produto/<int:id_produto>/', views.ver_produto, name='ver_produto'),
@@ -22,6 +24,11 @@ urlpatterns = [
     path('adicionarendereco/', views.adicionar_endereco, name='adicionar_endereco'),
     path('finalizarpedido/<int:id_pedido>/', views.finalizar_pedido, name='finalizar_pedido'),
     path('finalizarpagamento/', views.finalizar_pagamento, name='finalizar_pagamento'),
+    
+    #Conta
+    path('criarconta/', views.criar_conta, name='criar_conta'),
+    path('minhaconta/', views.minha_conta, name='minha_conta'),
+    path('recuperarsenha/', views.recuperar_senha, name='recuperar_senha'),
     path("password_change/", auth_views.PasswordChangeView.as_view(), name="password_change"),
     path("password_change/done/", auth_views.PasswordChangeDoneView.as_view(), name="password_change_done"),
     path("password_reset/", auth_views.PasswordResetView.as_view(), name="password_reset"),
